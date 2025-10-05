0183402afb2a6cf123c1393c90661c6871f362c0
Alexander Singer <xanderts24@gmail.com>
2025-10-05 15:38:25 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,11 +1,5 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
 `default_nettype none
-
-module tt_um_example (
+module tt_um_lexander_counter (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,10 +10,17 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+        reg [7:0] count;
+
+        always @(posedge clk)
+                if (!rst_n)
+                        count <= 8'b0;
+                else
+                        count <= count + 1;
+assign uo_out = count;
+
+assign uio_out = 0;
+assign uio_oe = 0;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
